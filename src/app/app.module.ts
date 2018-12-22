bc0f73831d03ffd706306c8d950b789513226893
tibiSzabo <bukkfabviszti@gmail.com>
2018-12-22 13:47:49 +0100

    fixed footer bottom margin bug (on phones) added new fonts adjusted some fonts for the header added animations for the header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ArticleListComponent } from './articles/article-list/article-list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ArticleListComponent
   ],
   imports: [
     BrowserModule,
