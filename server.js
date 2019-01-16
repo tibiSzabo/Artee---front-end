c14e69d99e0b7db754ffd6a96f5a607bf9afb1e1
Tibi <bukkfabiszti@gmail.com>
2019-01-16 17:26:55 +0100

    renamed the project, nothing else changed

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/egeszsegere'));
+app.use(express.static(__dirname + '/dist/artee'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/egeszsegere/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/artee/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
